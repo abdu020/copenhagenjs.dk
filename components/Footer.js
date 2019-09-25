ea0b9c816554b96c8bf1b9e00770c3559bb4b354
abdu020 <you@example.com>
2019-09-25 14:03:21 +0200

    Update videos data feed to with latest videos

--- components/Footer.js
+++ components/Footer.js
@@ -34,7 +34,7 @@ export default () => (
       >
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          width="50"
+          width="501"
           viewBox="0 0 256 180"
           style={{ verticalAlign: 'bottom' }}
         >
